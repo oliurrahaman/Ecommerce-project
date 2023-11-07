bef4b023f63e5eb73fbb9bfde6e2a05738b92375
oliurrahaman <bijoroliur1971@gmail.com>
2023-11-06 22:21:46 -0800

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Size;
+use App\Models\Unit;
+use App\Models\Brand;
+use App\Models\Color;
+use App\Models\Product;
+use App\Models\Category;
+use App\Models\SubCategory;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    private $subCategories;
+    public function index()
+    {
+        return view('admin.product.index', ['products' => Product::all()]);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.product.add', [
+            'categories' => Category::all(),
+            'sub_categories'=>SubCategory::all(),
+            'brands'=>Brand::all(),
+            'units'=>Unit::all(),
+            'colors'=>Color::all(),
+            'sizes'=>Size::all(),
+
+        ]);
+    }
+
+
+    public function getSubCategoryByCategory()
+    {
+        $id=$_GET['id'];
+        $this->subCategories =SubCategory::where('category_id',$_GET['id'])->get();
+        return response()->json($this->subCategories);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        return $request;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
